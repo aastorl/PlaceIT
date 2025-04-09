c9b5570e4688044f0c0e8fe4288e94f83a66555e
aastorl <aastorl79@gmail.com>
2025-04-09 15:10:36 -0300

    Initial Commit

---
+++ PlaceIT/PlaceITApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlaceITApp.swift
+//  PlaceIT
+//
+//  Created by Astor Ludueña  on 09/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct PlaceITApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

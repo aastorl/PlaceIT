a8c1f921fe1cd0c0bc060469e1836988f3153c68
aastorl <aastorl79@gmail.com>
2025-04-14 16:25:17 -0300

    Delete implemetnation for saved places

---
+++ PlaceIT/Model/Model.swift
@@ -0,0 +1,8 @@
+//
+//  Model.swift
+//  PlaceIT
+//
+//  Created by Astor Ludueña  on 09/04/2025.
+//
+
+import Foundation
---
+++ PlaceIT/Utilities/CustomDialogue.swift
@@ -0,0 +1,18 @@
+//
+//  CustomDialogue.swift
+//  PlaceIT
+//
+//  Created by Astor Ludueña  on 09/04/2025.
+//
+
+import SwiftUI
+
+struct CustomDialogue: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CustomDialogue()
+}
